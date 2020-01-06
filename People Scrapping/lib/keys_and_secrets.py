442f008f2e6cd1fc4e3028586f752e204f6770fa
Tomas Peluritis <tomas1991@gmail.com>
2020-01-06 07:25:39 +0200

    Delete keys_and_secrets.py

--- People Scrapping/lib/keys_and_secrets.py
+++
@@ -1,8 +0,0 @@
-# Follow https://cloud.google.com/docs/authentication/api-keys?authuser=1
-#  https://console.cloud.google.com/apis/credentials?authuser=1&_ga=2.230574364.-1828242701.1578038014
-# for API key ^
-
-#for CSE:
-# https://cse.google.com/cse/all
-api_key = "AIzaSyBUTSUZNm7PP7zrsLfk3cH-M5-pndXBodk"
-cse_id = "011020467613366574935:5wprhhovwxs"
